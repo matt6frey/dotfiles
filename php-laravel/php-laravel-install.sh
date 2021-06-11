9cba685137a6d05198521ad4ba428c2ed1844147
Matt Frey <matt6frey@gmail.com>
2021-06-10 19:33:14 -0600

    created initial install file for Laravel

---
+++ php-laravel/php-laravel-install.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+
+sudo apt -y install php7.4
+
+php -v
+
+sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
+
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+php composer.phar --version
+
+sudo mv composer.phar /usr/local/bin/composer
+
+composer --version
